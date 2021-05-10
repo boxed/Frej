5550af79f1fcfa013a2e21c05ece3995f40ed29b
Anders Hovmöller <anders.hovmoller@dryft.se>
2021-05-11 01:57:50 +0200

    Initial Commit

---
+++ SunHail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunHail
+//
+//  Created by Anders Hovmöller on 2021-05-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SunHail/SunHailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunHailApp.swift
+//  SunHail
+//
+//  Created by Anders Hovmöller on 2021-05-11.
+//
+
+import SwiftUI
+
+@main
+struct SunHailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
