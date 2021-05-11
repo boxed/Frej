143f4b4d5bbab77960d421a56b40091cd6861c14
Anders Hovmöller <anders.hovmoller@dryft.se>
2021-05-11 04:15:56 +0200

    initial

--- SunHail/ContentView.swift
+++ SunHail/ContentView.swift
@@ -7,10 +7,80 @@
 
 import SwiftUI
 
+
+func clockPathInner(path: inout Path, bounds: CGRect, progress: TimeInterval, extraSize: CGFloat = 1) {
+    let pi = Double.pi
+    let position: Double
+    position = pi - (2*pi * progress)
+    let size = bounds.height / 2
+    let x = bounds.midX + CGFloat(sin(position)) * size * extraSize
+    let y = bounds.midY + CGFloat(cos(position)) * size * extraSize
+    path.move(
+        to: CGPoint(
+            x: bounds.midX,
+            y: bounds.midY
+        )
+    )
+    path.addLine(to: CGPoint(x: x, y: y))
+}
+
+func clockPath(now: Date, bounds: CGRect, progress: Double, extraSize: CGFloat) -> Path {
+    Path { path in
+        clockPathInner(path: &path, bounds: bounds, progress: progress, extraSize: extraSize)
+    }
+}
+
+struct ClockDial: Shape {
+    var now: Date;
+    var progress: Double
+    var extraSize: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let now: Date = Date();
+        return clockPath(now: now, bounds: rect, progress: progress, extraSize: extraSize)
+    }
+}
+
+struct Clock : View {
+    var now: Date;
+    var body : some View {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([Calendar.Component.hour, Calendar.Component.minute, Calendar.Component.second, Calendar.Component.nanosecond], from: now)
+        let hour = Double(components.hour!)
+        let minutes = Double(components.minute!)
+        let seconds = Double(components.second!) + Double(components.nanosecond!) / 1_000_000_000.0
+
+
+        ZStack {
+            ClockDial(now: now, progress: hour / 12, extraSize: 0.4).stroke(Color.white, style: StrokeStyle(lineWidth: 8, lineCap: .round))
+            ClockDial(now: now, progress: minutes / 60.0, extraSize: 0.9).stroke(Color.white, style: StrokeStyle(lineWidth: 4, lineCap: .round))
+            ClockDial(now: now, progress: seconds / 60.0, extraSize: 0.9).stroke(Color.white, style: StrokeStyle(lineWidth: 2, lineCap: .round))
+        }
+    }
+}
+
 struct ContentView: View {
+    @State var now: Date = Date()
+
+    let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
+
+    var clock: some View {
+        VStack {
+            Clock(now: now)
+            .onReceive(timer) { input in
+                now = input
+            }
+            .background(Circle().stroke(Color.white)).foregroundColor(Color.white)
+            .padding(50)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            clock
+            clock
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
