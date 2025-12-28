5cdd002ca745da4724dcdc84bed44264ff52b7a3
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-12-28 02:20:22 +0100

    .

--- Frej/FrejView.swift
+++ Frej/FrejView.swift
@@ -923,7 +923,7 @@ struct FrejView: View {
                         Button(action: { showSettings = true }) {
                             Image(systemName: "gearshape.fill")
                                 .font(.system(size: 24))
-                                .foregroundColor(.white.opacity(0.7))
+                                .foregroundColor(.white.opacity(0.2))
                                 .padding(12)
                                 .background(Circle().fill(Color.black.opacity(0.5)))
                         }
